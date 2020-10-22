21756feea06de9a3ae6b12b22eadd791c2b37d50
rudyrim <47420295+rudyrim@users.noreply.github.com>
2020-10-22 20:34:27 +0200

    update

--- exercises/chess/cmd/chess/main.go
+++ exercises/chess/cmd/chess/main.go
@@ -4,11 +4,33 @@ import (
 	"fmt"
 )
 
+var (
+	a = [8]string{"♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"}
+	b = [8]string{"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"}
+	c = [8]string{" ", " ", " ", " ", " ", " ", " ", " "}
+	d = [8]string{" ", " ", " ", " ", " ", " ", " ", " "}
+	e = [8]string{" ", " ", " ", " ", " ", " ", " ", " "}
+	f = [8]string{" ", " ", " ", " ", " ", " ", " ", " "}
+	g = [8]string{"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"}
+	h = [8]string{"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"}
+)
+
+func display() {
+	fmt.Println(" ", " 0", "1", "2", "3", "4", "5", "6", "7")
+	fmt.Println("A", a, "A")
+	fmt.Println("B", b, "B")
+	fmt.Println("C", c, "C")
+	fmt.Println("D", d, "D")
+	fmt.Println("E", e, "E")
+	fmt.Println("F", f, "F")
+	fmt.Println("G", g, "G")
+	fmt.Println("H", h, "H")
+	fmt.Println(" ", " 0", "1", "2", "3", "4", "5", "6", "7")
+}
 func main() {
-	const(
-	ligne int = 2
-	colonne int =6
-	)
-	var tab = [ligne][colonne] string {{"♙","♘","♗","♖","♕","♔"},{"♟","♞","♝","♜","♛","♚"}}
-	fmt.Println(tab[1][5])
+
+	e[1] = "♙"
+	b[1] = " "
+	display()
+
 }
